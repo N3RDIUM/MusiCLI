387ac6c640a15337130253892c8a458665f2f4ce
somePythonProgrammer <74598401+somePythonProgrammer@users.noreply.github.com>
2022-04-29 19:36:53 +0530

    Scrolling text selector!

--- screens/homescreen.py
+++ screens/homescreen.py
@@ -1,39 +1,18 @@
 from ui_elements import *
+from pynput.keyboard import Key, Listener
 
 class HomeScreen:
     def __init__(self, parent):
         self.parent = parent
-        self.latest_key = None
+        self.scrolling_text = ScrollingSelector(self, ["How are you?", "MusiCLI is a music player for Windows.", "Press enter to start.", "How are you?", "MusiCLI is a music player for Windows.", "Press enter to start.", "How are you?", "MusiCLI is a music player for Windows.", "Press enter to start.", ])
+        self.key = None
 
-        self.scrolling_text = ScrollingText(self, "Hello World! You are using a MusiCLI Preview version!")
+        self.listener = Listener(on_press=self.on_key_press)
+        self.listener.start()
 
     def on_key_press(self, key):
-        try:
-            self.latest_key = key.char
-        except:
-            pass
+        self.scrolling_text.on_key_press(key)
+        self.key = key
 
     def render(self, dimensions):
-        # center the text
-        x = int(dimensions[0] / 2)
-        y = int(dimensions[1] / 2)
-
-        t = self.scrolling_text.render()
-
-        width = len(t)
-        height = 1
-
-        string = ""
-        
-        for i in range(0, int(y / 2)):
-            string += "\n"
-        
-        for i in range(0, int(x / 2) - width):
-            string += " "
-        
-        string += t + "" if self.latest_key is None else self.latest_key
-
-        for i in range(0, int(y / 2)):
-            string += "\n"
-
-        return string
+        return self.scrolling_text.render() + "\n" + str(self.key)
--- ui_elements.py
+++ ui_elements.py
@@ -1,6 +1,16 @@
+from pynput.keyboard import Key
+
 class UIElementBase():
     def __init__(self, parent):
         self.parent = parent
+
+class Text(UIElementBase):
+    def __init__(self, parent, text):
+        super().__init__(parent)
+        self.text = text
+
+    def render(self):
+        return self.text
 
 DEFAULT_SCROLLING_TEXT_LENGTH = 25
 
@@ -30,3 +40,54 @@ class ScrollingText(UIElementBase):
 
         return "[" + string + "]"
 
+class ScrollingSelector(UIElementBase):
+    def __init__(self, parent, options, selected_index=0, heightrange = 2):
+        super().__init__(parent)
+        self.options = options
+        self.selected_index = selected_index
+        self.heightrange = heightrange
+        self.frame = 0
+
+        self.selected = None
+
+        self.current_animation = [">", "-", "—", "-"]
+        self.current_animation_index = 0
+
+    def on_key_press(self, key):
+        if key == Key.up:
+            self.selected_index -= 1
+            if self.selected_index < 0:
+                self.selected_index = len(self.options) - 1
+        elif key == Key.down:
+            self.selected_index += 1
+            if self.selected_index >= len(self.options):
+                self.selected_index = 0
+        elif key == Key.enter:
+            self.selected = self.options[self.selected_index]
+
+    def render(self):
+        # increment frame
+        self.frame += 1
+
+        if self.frame % 400 == 0:
+            self.current_animation_index += 1
+
+            if self.current_animation_index >= len(self.current_animation) - 1:
+                self.current_animation_index = 0
+
+        string = ""
+
+        _range = range(self.selected_index - self.heightrange, self.selected_index + self.heightrange + 1)
+        for i in _range:
+            if i < 0 or i >= len(self.options):
+                continue
+
+            if i == self.selected_index:
+                string += self.current_animation[self.current_animation_index] + " " + self.options[i] + "\n"
+            else:
+                string += "  " + self.options[i] + "\n"
+
+        if len(string) < self.heightrange * 2 + 1:
+            string = "  " * (self.heightrange - len(_range) / 2) + string
+
+        return string
