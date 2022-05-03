b15335349133d7e0ef276f40b928340345f34f0f
somePythonProgrammer <74598401+somePythonProgrammer@users.noreply.github.com>
2022-05-03 15:35:14 +0530

    ...

--- screens/homescreen.py
+++ screens/homescreen.py
@@ -3,6 +3,12 @@ from pynput.keyboard import Key, Listener
 import curses
 from curses import panel
 
+def make_panel(x, y, width, height):
+    _panel = curses.newwin(height, width, y, x)
+    _panel = panel.new_panel(_panel)
+    _panel.top()
+    return _panel
+
 class HomeScreen:
     def __init__(self, parent):
         self.parent = parent
@@ -10,7 +16,8 @@ class HomeScreen:
 
         # Make all the panels
         self.panels = {
-            "side-navbar": panel.new_panel(self.parent.parent.stdscr),
+            "side-navbar": make_panel(0, 0, 30, self.dimensions[0]),
+            "main-content": make_panel(30, 0, self.dimensions[1] - 30, self.dimensions[0])
         }
 
         self.listener = Listener(on_press=self.on_key_press)
@@ -24,13 +31,14 @@ class HomeScreen:
     def render(self):
         self.dimensions = self.parent.parent.stdscr.getmaxyx()
 
-        self.panels["side-navbar"] = panel.new_panel(self.parent.parent.stdscr)
         win = self.panels["side-navbar"].window()
-        win.resize(self.dimensions[0], 30)
-
-        win.clear()
         win.border()
-        win.addstr(1, 1, str(self.num))
-        self.num += 1
+        win.addstr(1, 1, "Sidebar")
+        win.refresh()
 
-        return "SDF"
+        win = self.panels["main-content"].window()
+        win.border()
+        win.addstr(1, 1, "Home")
+        win.refresh()
+
+        return ""
